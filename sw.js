f815190ae647177f5b32b325742c04b67ed95de5
sppgtambakrejo <asistenlapangantambakrejo@gmail.com>
2026-05-15 23:49:31 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,40 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = "absensi-sppg-v1";
 
-  console.log("Service Worker Installed");
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./login.html",
+  "./admin.html",
+  "./style.css",
+  "./manifest.json"
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
 
-
-self.addEventListener("fetch", event => {
-
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
